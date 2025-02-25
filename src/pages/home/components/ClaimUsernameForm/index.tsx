6b91f3fe26c0e3fc3b2e136dacf1a4df76f28ca8
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-25 08:16:03 -0300

    default value username

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -7,6 +7,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
 
 const claimUIsernameFormSchema = z.object({
   username: z
@@ -21,16 +22,18 @@ const claimUIsernameFormSchema = z.object({
 type ClaimUsernameFormData = z.infer<typeof claimUIsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const router = useRouter()
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUIsernameFormSchema),
   })
 
   async function handlePreRegister(data: ClaimUsernameFormData) {
-    console.log(data)
+    const { username } = data
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -42,7 +45,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuario"
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
