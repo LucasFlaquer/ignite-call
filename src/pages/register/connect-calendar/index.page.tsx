d6172660d1050c19ebe764e50f7112dc754e69d1
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-25 19:25:21 -0300

    connect next-auth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -3,11 +3,14 @@ import {
   ConnectItem,
 } from '@/pages/register/connect-calendar/styles'
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn, useSession } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 
 import { Container, Header } from '../styles'
 
 export default function Register() {
+  const session = useSession()
+
   return (
     <Container>
       <Header>
@@ -21,7 +24,11 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar <ArrowRight />
           </Button>
         </ConnectItem>
