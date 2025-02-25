7b5116d26ba1928e7836afd31d5ad7d669dfa290
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-24 22:59:15 -0300

    add nvmrc

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,17 @@
 import { Form } from '@/pages/home/components/ClaimUsernameForm/styles'
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 
 
 export function ClaimUsernameForm() {
+  const { register } = useForm()
   return (
     <Form as="form">
-      <TextInput
-        size="sm"
+      <TextInput size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
