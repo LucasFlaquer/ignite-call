6743aa9c70c8755d3b9508df786872a8d8899605
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-25 17:09:38 -0300

    connect to calendar

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,35 @@
+import {
+  ConnectBox,
+  ConnectItem,
+} from '@/pages/register/connect-calendar/styles'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Header } from '../styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
