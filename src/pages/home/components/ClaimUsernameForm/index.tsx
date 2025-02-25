3e271e87e600c66bfb9f7c7e107d78ead48adc8d
lucasflaquer <lucas.flaquer@gmail.com>
2025-02-24 23:00:26 -0300

    chore: downgrade

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -3,12 +3,12 @@ import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 
-
 export function ClaimUsernameForm() {
   const { register } = useForm()
   return (
     <Form as="form">
-      <TextInput size="sm"
+      <TextInput
+        size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
         {...register('username')}
