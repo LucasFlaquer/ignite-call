e97415ecffdb52286cb33d2e6ee6a218c3dad2ca
lucasflaquer <lucas.flaquer@gmail.com>
2025-03-01 00:55:36 -0300

    time intervals save endpoint

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,7 +1,19 @@
+import { prisma } from '@/lib/prisma'
 import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api'
 import { NextApiRequest, NextApiResponse } from 'next'
 // eslint-disable-next-line camelcase
 import { unstable_getServerSession } from 'next-auth'
+import { z } from 'zod'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,5 +25,19 @@ export default async function handler(
     res,
     buildNextAuthOptions(req, res),
   )
-  return res.json({ session })
+  if (!session) return res.status(401).end()
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+  return res.status(201).end()
 }
