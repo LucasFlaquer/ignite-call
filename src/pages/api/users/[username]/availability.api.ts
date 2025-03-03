40d5f62a69630d9dba7fd739ce29160a59b2a0e7
lucasflaquer <lucas.flaquer@gmail.com>
2025-03-03 16:56:26 -0300

    connect to api

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -11,17 +11,18 @@ export default async function handle(
   const { date } = req.query
   if (!date) return res.status(400).json({ message: 'Date no provided.' })
   const user = await prisma.user.findUnique({ where: { username } })
-  if (!user) return res.status(400).json({ message: 'User does not exist' })
+  if (!user) return res.status(400).json({ message: 'User does not exist.' })
   const referenceDate = dayjs(String(date))
   const isPastDate = referenceDate.endOf('day').isBefore(new Date())
-  if (isPastDate) return res.json({ availability: [] })
+  if (isPastDate) return res.json({ availableTimes: [], possibleTimes: [] })
   const userAvailability = await prisma.userTimeInterval.findFirst({
     where: { user_id: user.id, week_day: referenceDate.get('day') },
   })
-  if (!userAvailability) return res.json({ availability: [] })
+  if (!userAvailability)
+    return res.json({ availableTimes: [], possibleTimes: [] })
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability
-  const startHour = time_start_in_minutes * 60
-  const endHour = time_end_in_minutes * 60
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
   const possibleTimes = Array.from({ length: endHour - startHour }).map(
     (_, i) => {
       return startHour + i
