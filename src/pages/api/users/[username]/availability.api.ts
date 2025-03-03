7422a542f9bfb8975f56cd090a262a145a561907
lucasflaquer <lucas.flaquer@gmail.com>
2025-03-03 16:42:04 -0300

    fix grammar

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -9,7 +9,7 @@ export default async function handle(
   if (req.method !== 'GET') return res.status(405).end()
   const username = String(req.query.username)
   const { date } = req.query
-  if (!date) return res.status(400).json({ message: 'Date not provided' })
+  if (!date) return res.status(400).json({ message: 'Date no provided.' })
   const user = await prisma.user.findUnique({ where: { username } })
   if (!user) return res.status(400).json({ message: 'User does not exist' })
   const referenceDate = dayjs(String(date))
